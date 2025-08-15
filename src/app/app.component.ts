e3ad93430a2ad9246b8ec26dc4287220ce269d83
Gabriel Luiz <gabriellnsilva2004@gmail.com>
2025-08-15 20:49:12 -0300

    Adição de Separador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './components/container/container.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './components/separador/separador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, CabecalhoComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 }
